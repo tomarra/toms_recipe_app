db7a8a82e436e83e936d94828fe25187782fdcd4
Tom Arra <tarra3@gmail.com>
2025-06-10 08:50:05 -0500

    fix: update app title (#20)

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recipes App',
+      title: 'Tom\'s Recipes App',
       theme: ThemeData(colorSchemeSeed: Colors.lightGreen, useMaterial3: true),
       home: const RecipeListPage(),
     );
