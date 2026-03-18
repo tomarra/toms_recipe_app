2dacb456e72de02942c88f8849e3903b2829f212
Tom Arra <tarra3@gmail.com>
2026-03-18 08:03:37 -0500

    feat: upgrade to flutter 3.41.4

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tom\'s Recipes App',
+      title: 'Toms Recipes App',
       theme: ThemeData(colorSchemeSeed: Colors.red, useMaterial3: true),
       home: const RecipeListPage(),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:toms_recipe_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
